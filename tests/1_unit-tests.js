1a276486b08859d3a388689ff318937649668ad6
rahimasalmanova <rahima@artifactlabs.io>
2022-05-13 12:51:09 +0400

    solve 4th assigment

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -25,12 +25,12 @@ suite("Unit Tests", function () {
     });
     // #4
     test("#isTrue, #isNotTrue", function () {
-      assert.fail(true, "true is true");
-      assert.fail(
+      assert.isTrue(true, "true is true");
+      assert.isTrue(
         !!"double negation",
         "Double negation of a truthy value is true"
       );
-      assert.fail(
+      assert.isNotTrue(
         { value: "truthy" },
         "Objects are truthy, but are not boolean values"
       );
