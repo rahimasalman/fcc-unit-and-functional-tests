0d770fe4fe862b163f1f7c09d26d5bc61e021bb9
rahimasalmanova <rahima@artifactlabs.io>
2022-05-15 00:57:39 +0400

    solve 10th assigment

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -108,11 +108,11 @@ suite("Unit Tests", function () {
   suite("Arrays", function () {
     // #11
     test("#isArray, #isNotArray", function () {
-      assert.fail(
+      assert.isArray(
         "isThisAnArray?".split(""),
         "String.prototype.split() returns an array"
       );
-      assert.fail([1, 2, 3].indexOf(2), "indexOf returns a number");
+      assert.isNotArray([1, 2, 3].indexOf(2), "indexOf returns a number");
     });
     // #12
     test("Array #include, #notInclude", function () {
