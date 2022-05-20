fa5afad0deb3756e0fecd0548405ad7ea539e7be
rahimasalman <rehimesalman7@gmail.com>
2022-05-20 20:17:37 +0400

    solve functional tests:(1st, 2nd, 3rd one) using zombie.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,44 +8,62 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
   this.timeout(5000);
-  suite('Integration tests with chai-http', function () {
+    suite('Integration tests with chai-http', function () {
     // #1
     test('Test GET /hello with no name', function (done) {
       chai
         .request(server)
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
     // #2
     test('Test GET /hello with your name', function (done) {
-      chai
+        chai
         .request(server)
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });
     // #3
     test('Send {surname: "Colombo"}', function (done) {
-      chai
+     chai
         .request(server)
         .put('/travellers')
-
+        .send (
+          {"surname": "Colombo"},
+          {"type": 'application/json'}
+        )
+        
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+     chai
+        .request(server)
+        .put('/travellers')
+        .send (
+          {"surname": "da Verrazzano"},
+          {"type": 'application/json'}
+        )
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano');
+        });
 
       done();
     });
@@ -53,6 +71,8 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+Browser.site = 'https://boilerplate-mochachai.rahimasalman.repl.co';
+const browser = new Browser();
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
@@ -61,7 +81,9 @@ suite('Functional Tests with Zombie.js', function () {
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
+      suiteSetup(function(done) {
+      return browser.visit('/', done);
+      });
     });
   });
 
